2c8e142468ffb609a7eebdb9ae9f996a8481adee
jemmy9211 <jemmy9211@gmail.com>
2025-02-22 21:10:05 +0800

    Refactor commit message generation with increased creativity and option to regenerate with new messages.

--- src/generate_message.py
+++ src/generate_message.py
@@ -2,13 +2,39 @@ import subprocess
 from langchain.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
 
-# Configure LLM with local Ollama instance
-llm = OllamaLLM(model="llama3.2", base_url="http://localhost:11434")
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "Your task is to write a concise commit message according to a given code diff. Your output should only be the commit message with no other information."),
-    ("user", "Code Diff: {query_diff} Commit Message:")
-])
-chain = prompt | llm
+class MessageGenerator:
+    def __init__(self):
+        self.base_temperature = 0.7
+        self.temperature_increment = 0.1
+        self.max_temperature = 1.0
+        self.current_temperature = self.base_temperature
+        
+    def create_llm(self):
+        return OllamaLLM(
+            model="llama3.2", 
+            base_url="http://localhost:11434",
+            temperature=self.current_temperature
+        )
+
+    def increase_temperature(self):
+        self.current_temperature = min(
+            self.current_temperature + self.temperature_increment,
+            self.max_temperature
+        )
+        
+    def reset_temperature(self):
+        self.current_temperature = self.base_temperature
+
+    def generate_message(self, diff):
+        llm = self.create_llm()
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "Your task is to write a concise commit message according to a given code diff. Your output should only be the commit message with no other information."),
+            ("user", "Code Diff: {query_diff} Commit Message:")
+        ])
+        chain = prompt | llm
+        return chain.invoke({"query_diff": diff})
+
+generator = MessageGenerator()
 
 def get_diff():
     """Get the Git diff of staged changes."""
@@ -20,8 +46,15 @@ def get_diff():
 
 def generate_message(qdiff):
     """Generate a commit message using the Ollama LLM."""
-    rsp = chain.invoke({"query_diff": qdiff})
-    return rsp
+    return generator.generate_message(qdiff)
+
+def increase_creativity():
+    """Increase the temperature for next generation."""
+    generator.increase_temperature()
+
+def reset_creativity():
+    """Reset the temperature to base value."""
+    generator.reset_temperature()
 
 def main():
     """Main function to generate commit message."""
